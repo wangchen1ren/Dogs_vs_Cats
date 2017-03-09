913d39d117933203bfa6b67d58ba8c32d2861fc8
FroZenpiG <wangchen1ren@gmail.com>
2017-03-09 17:30:45 +0800

    update

---
+++ image_utils.py
@@ -0,0 +1,37 @@
+import numpy as np
+from skimage import transform
+
+MEAN_PIXEL = [127, 127, 127]
+
+
+def crop_image(image, dshape):
+    factor = float(min(dshape[:2])) / min(image.shape[:2])
+    new_size = [int(image.shape[0] * factor), int(image.shape[1] * factor)]
+    if new_size[0] < dshape[0]:
+        new_size[0] = dshape[0]
+    if new_size[1] < dshape[0]:
+        new_size[1] = dshape[0]
+    resized_image = transform.resize(image, new_size)
+    sample = np.asarray(resized_image) * 256
+    if dshape[0] < sample.shape[0] or dshape[1] < sample.shape[1]:
+        xx = int((sample.shape[0] - dshape[0]))
+        yy = int((sample.shape[1] - dshape[1]))
+        xstart = xx / 2
+        ystart = yy / 2
+        xend = xstart + dshape[0]
+        yend = ystart + dshape[1]
+        sample = sample[xstart:xend, ystart:yend, :]
+    return sample
+
+
+def preprocess_image(path, dshape):
+    image = imread(path)
+    image = crop_image(image, dshape=dshape)
+    image -= MEAN_PIXEL
+    return image
+
+
+def postprocess_image(image, dshape):
+    image = np.reshape(image, dshape) + MEAN_PIXEL
+    image = np.clip(image, 0, 255).astype(np.uint8)
+    return image
--- train.py
+++ train.py
@@ -1,52 +1,16 @@
 import glob
 import os
 
-import numpy as np
 import tflearn
-from skimage import transform
-from skimage.io import imread
 from sklearn.model_selection import train_test_split
+
+from image_utils import preprocess_image
 
 pjoin = os.path.join
 TRAIN_DATA = pjoin(os.path.dirname(__file__), 'images')
 MODEL_PATH = pjoin(os.path.dirname(__file__), 'model')
 if not os.path.exists(MODEL_PATH):
     os.makedirs(MODEL_PATH, mode=0755)
-
-MEAN_PIXEL = [127, 127, 127]
-
-
-def crop_image(image, dshape):
-    factor = float(min(dshape[:2])) / min(image.shape[:2])
-    new_size = [int(image.shape[0] * factor), int(image.shape[1] * factor)]
-    if new_size[0] < dshape[0]:
-        new_size[0] = dshape[0]
-    if new_size[1] < dshape[0]:
-        new_size[1] = dshape[0]
-    resized_image = transform.resize(image, new_size)
-    sample = np.asarray(resized_image) * 256
-    if dshape[0] < sample.shape[0] or dshape[1] < sample.shape[1]:
-        xx = int((sample.shape[0] - dshape[0]))
-        yy = int((sample.shape[1] - dshape[1]))
-        xstart = xx / 2
-        ystart = yy / 2
-        xend = xstart + dshape[0]
-        yend = ystart + dshape[1]
-        sample = sample[xstart:xend, ystart:yend, :]
-    return sample
-
-
-def preprocess_image(path, dshape):
-    image = imread(path)
-    image = crop_image(image, dshape=dshape)
-    image -= MEAN_PIXEL
-    return image
-
-
-def postprocess_image(image, dshape):
-    image = np.reshape(image, dshape) + MEAN_PIXEL
-    image = np.clip(image, 0, 255).astype(np.uint8)
-    return image
 
 
 def read_data():
@@ -88,6 +52,7 @@ net = tflearn.global_avg_pool(net)
 # Regression
 net = tflearn.fully_connected(net, 2, activation='softmax')
 net = tflearn.regression(net, optimizer='adam', loss='categorical_crossentropy')
+
 # Training
 model = tflearn.DNN(net, checkpoint_path='model_resnet',
                     max_checkpoints=10, tensorboard_verbose=0,
